2e2d464c68e6fa945b3c730be3b6afd2577b69b4
Niklas Köhler <niklas.koehler@posteo.de>
2015-03-16 09:57:32 +0100

    Encapsulated the main logic into a main function
    - Error handling is now possible with i.e:
        try:
            main()
        execption BaseException:
            sys.exit(1)
    - Important: The global variables at the top are essential to run the
      program properly, they can't be definied inside the main function
    - You can now run G in interactive mode or G splits an commandline
      argument, to get the arguments. You can now invoke G like the
      following examples, directly from your prompt:
        G "@master -> @origin"

--- G.py
+++ G.py
@@ -3,19 +3,9 @@
 import re, subprocess, sys
 from os.path import basename
 
-try:
-    userInput = input( "G: " )
-except BaseException:
-    sys.exit( 1 )
-
-args = userInput.split()
 data = { "add": [], "reset": [] }
 operator = ""
 branches = []
-
-# try:
-#     subprocess.check_call( "git symbolic-ref HEAD", shell=True )
-#     currentBranch = basename(  )
 
 def showUsage():
     usage = """
@@ -49,10 +39,7 @@ def isBranch( possibleBranch ):
         return False
 
 def git( cmd, files ):
-    try:
-        subprocess.call( [ "git", cmd ] + files )
-    except BaseException:
-        sys.exit(1)
+    subprocess.call( [ "git", cmd ] + files )
 
 def parseArgs( args ):
 
@@ -65,18 +52,18 @@ def parseArgs( args ):
     for arg in args:
         index = args.index( arg )
         if index < length:
-            if index == 0:
-                if arg == "+":
-                    operator = "add"
-                elif arg == "-":
-                    operator = "reset"
-            elif index >= 0:
+            if index >= 0:
                 if arg == "->":
                     operator = "push"
                 elif arg == ">":
                     operator = "merge"
                 elif isBranch( arg ):
                     branches.append( arg[1:] )
+            elif index == 0:
+                if arg == "+":
+                    operator = "add"
+                elif arg == "-":
+                    operator = "reset"
             elif index > 0:
                 if arg == "+":
                     del args[0:index]
@@ -94,24 +81,39 @@ def parseArgs( args ):
             elif isPath( arg ):
                 data[operator].append( arg )
 
-if not args:
-    showUsage()
-else:
-    parseArgs( args )
+def main():
 
-if data.get( "add" ) != []:
-    git( "add", data.get( "add" ) )
-elif data.get( "reset" ) != []:
-    git( "reset", data.get( "reset" ) )
+    if len( sys.argv ) == 1:
+        rawInput = input( "G: " )
+    else:
+        rawInput = sys.argv[1]
 
-if operator == "push":
-    if len( branches ) == 1:
-        git( "push", [ "origin", branches[0] ] )
-    elif len( branches ) == 2:
-        git( "push", [ branches[1], branches[0] ] )
-elif operator == "merge":
-    if len( branches ) == 1:
-        git( "merge", [ branches[0] ] )
-    elif len( branches ) == 2:
-        git( "checkout",  [ branches[0] ]  )
-        git( "merge", branches[1] )
+    args = rawInput.split()
+
+    if not args:
+        showUsage()
+    else:
+        parseArgs( args )
+
+    if data.get( "add" ) != []:
+        git( "add", data.get( "add" ) )
+    elif data.get( "reset" ) != []:
+        git( "reset", data.get( "reset" ) )
+
+    if operator == "push":
+        if len( branches ) == 1:
+            git( "push", [ "origin", branches[0] ] )
+        elif len( branches ) == 2:
+            git( "push", [ branches[1], branches[0] ] )
+    elif operator == "merge":
+        if len( branches ) == 1:
+            git( "merge", [ branches[0] ] )
+        elif len( branches ) == 2:
+            git( "checkout",  [ branches[0] ]  )
+            git( "merge", branches[1] )
+
+if __name__ == "__main__":
+    try:
+        main()
+    except BaseException:
+        sys.exit(1)
