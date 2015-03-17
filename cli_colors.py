9e85cea86980e8eb1bc86a4c3a067a35eb3dfedb
Niklas Köhler <niklas.koehler@posteo.de>
2015-03-17 20:12:02 +0100

    Added the cli_colors module for color escape codes
    I wrote a simple class fg which generates the specific Escape-Codes for
    the terminal.
    
    For example the following code:
        return( "Hello " + fg.red( "World!" ) )
    produces the following string:
        "Hello \033[31mWorld\033[0m"
    
    That's much prettier, he?

---
+++ cli_colors.py
@@ -0,0 +1,18 @@
+class fg():
+    def black( text ):
+        return "\033[30m" + text + "\033[0m"
+    def red( text ):
+        return "\033[31m" + text + "\033[0m"
+    def green( text ):
+        return "\033[32m" + text + "\033[0m"
+    def yellow( text ):
+        return "\033[33m" + text + "\033[0m"
+    def blue( text ):
+        return "\033[34m" + text + "\033[0m"
+    def violett( text ):
+        return "\033[34m" + text + "\033[0m"
+    def cyan( text ):
+        return "\033[35m" + text + "\033[0m"
+    def white( text ):
+        return "\033[36m" + text + "\033[0m"
+
