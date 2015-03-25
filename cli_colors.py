6c0772350846685fb131709726d7678343620feb
Niklas Köhler <niklas.koehler@posteo.de>
2015-03-25 13:57:16 +0100

    Added the fg and style class to the cli_colors module

--- cli_colors.py
+++ cli_colors.py
@@ -10,9 +10,36 @@ class fg():
     def blue( text ):
         return "\033[34m" + text + "\033[0m"
     def violett( text ):
-        return "\033[34m" + text + "\033[0m"
-    def cyan( text ):
         return "\033[35m" + text + "\033[0m"
-    def white( text ):
+    def cyan( text ):
         return "\033[36m" + text + "\033[0m"
+    def white( text ):
+        return "\033[37m" + text + "\033[0m"
 
+class bg():
+    def black( text ):
+        return "\033[40m" + text + "\033[0m"
+    def red( text ):
+        return "\033[41m" + text + "\033[0m"
+    def green( text ):
+        return "\033[42m" + text + "\033[0m"
+    def yellow( text ):
+        return "\033[43m" + text + "\033[0m"
+    def blue( text ):
+        return "\033[44m" + text + "\033[0m"
+    def violett( text ):
+        return "\033[45m" + text + "\033[0m"
+    def cyan( text ):
+        return "\033[46m" + text + "\033[0m"
+    def white( text ):
+        return "\033[47m" + text + "\033[0m"
+
+class style():
+    def bold( text ):
+        return "\033[1m" + text + "\033[0m"
+    def italic( text ):
+        return "\033[3m" + text + "\033[0m"
+    def underlined( text ):
+        return "\033[4m" + text + "\033[0m"
+    def blink( text ):
+        return "\033[4m" + text + "\033[0m"
